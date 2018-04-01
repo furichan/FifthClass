e00ed5761630adff62453638ae3457eb48dd382f
Administrator <Administrator@windows10.microdone.cn>
2018-04-01 15:21:15 +0800

    This demo is dedicated for demonstrating,private only!

---
+++ src/test/java/com/bbc/MavenTestngDemo/AppTest.java
@@ -0,0 +1,14 @@
+package com.bbc.MavenTestngDemo;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void MavenTestngDemo() {
+		System.out.println("测试");
+	}
+
+}
